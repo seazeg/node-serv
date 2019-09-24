6dee8d661cdb33468407954b483143fe29b577b6
Evan.G <seazeg@126.com>
2019-09-24 20:11:30 +0800

    结构修改

---
+++ app/src/screenshot/core.js
@@ -0,0 +1,86 @@
+import puppeteer from 'puppeteer';
+import { resolve as _resolve } from 'path';
+import devices from './DeviceDescriptors';
+import { guid } from 'eg-utils';
+import { yellow } from 'chalk';
+import dirExists from './utils';
+import { daoLogger } from '../../../g2m/lib'
+const resolve = file => _resolve(__dirname, file)
+
+const config = {
+    headless: true,
+    timeout: 30000
+}
+ 
+export default async function run({
+    hostName,
+    devic = '14PC',
+    storagePath = resolve('../../screenshot/')
+} = {}) {
+    try {
+        daoLogger('app:screenshot').info(yellow("正在获取页面信息.."))
+        const browser = await puppeteer.launch({
+            // executablePath: config.executablePath,
+            headless: config.headless,
+            timeout: config.timeout,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+        await page.emulate(devices[devic])
+        await page.goto(`http://${hostName}`, {
+            timeout: config.timeout,
+            waitUntil: 'networkidle2'
+        })
+        await autoScroll(page);
+        daoLogger('app:screenshot').info(yellow("开始截图.."))
+        dirExists(storagePath)
+        let tempName = guid();
+        await page.screenshot({
+            path: `${storagePath}/${tempName}.jpeg`,
+            quality: 100,
+            fullPage: true,
+            type: 'jpeg'
+        });
+        daoLogger('app:screenshot').info(yellow("完成截图.."))
+        await browser.close();
+        daoLogger('app:screenshot').info(yellow("done!"))
+        return {
+            data: {
+                fileName: `${tempName}.jpeg`
+            },
+            isSuccess: true,
+            resultMsg: "执行成功！"
+        }
+    } catch (error) {
+        daoLogger('app:screenshot').error(error)
+        let errorlst = {
+            data: "",
+            isSuccess: false,
+            resultMsg: "执行失败！"
+        };
+        if (error.code == 'ENOENT') {
+            errorlst.resultMsg = "目录无效"
+        }
+        return errorlst
+    }
+}
+
+
+async function autoScroll(page) {
+    return page.evaluate(() => {
+        return new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+                if (totalHeight >= scrollHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        })
+    });
+}
+
---
+++ app/src/screenshot/utils.js
@@ -0,0 +1,52 @@
+import { stat, mkdir as _mkdir } from 'fs';
+import { parse } from 'path';
+import { daoLogger } from '../../../g2m/lib'
+
+function getStat(path) {
+    return new Promise((resolve, reject) => {
+        stat(path, (err, stats) => {
+            if (err) {
+                daoLogger('app:screenshot').error('目标目录不存在..')
+                resolve(false);
+            } else {
+                resolve(stats);
+            }
+        })
+    })
+}
+
+
+function mkdir(dir) {
+    return new Promise((resolve, reject) => {
+        _mkdir(dir, err => {
+            if (err) {
+                resolve(false);
+            } else {
+                daoLogger('app:screenshot').info('目录创建成功..')
+                resolve(true);
+            }
+        })
+    })
+}
+
+
+async function dirExists(dir) {
+    let isExists = await getStat(dir);
+    //如果该路径且不是文件，返回true
+    if (isExists && isExists.isDirectory()) {
+        return true;
+    } else if (isExists) { //如果该路径存在但是文件，返回false
+        return false;
+    }
+    //如果该路径不存在
+    let tempDir = parse(dir).dir; //拿到上级路径
+    //递归判断，如果上级目录也不存在，则会代码会在此处继续循环执行，直到目录存在
+    let status = await dirExists(tempDir);
+    let mkdirStatus;
+    if (status) {
+        mkdirStatus = await mkdir(dir);
+    }
+    return mkdirStatus;
+}
+
+export default dirExists
