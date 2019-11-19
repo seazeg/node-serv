b1bd208dc14b9dbb79f3beac22792d32309d35c9
Evan.G <seazeg@126.com>
2019-11-19 11:18:46 +0800

    Default Commit Message

--- g2m/lib/server/serv.js
+++ g2m/lib/server/serv.js
@@ -8,6 +8,12 @@ var _app = require('./app');
 
 var _logger = require('../logger');
 
+var _easyMonitor = require('easy-monitor');
+
+var _easyMonitor2 = _interopRequireDefault(_easyMonitor);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 const PORT = process.env.PORT || 5257;
 
 const run = () => {
@@ -15,7 +21,22 @@ const run = () => {
   (0, _logger.loggerConfig)({
     dirname: process.env.LOGGER_PATH
   });
-
+  (0, _easyMonitor2.default)({
+    project_name: 'G2M',
+    auth: {
+      need: true,
+      /**
+       @param {string} user 用户名
+       @param {string} pass 用户键入密码
+       @return {promise}
+       **/
+      authentication(user, pass) {
+        return new Promise(resolve => {
+          if (user === 'zhouergeng' && pass === 'trsadmin') resolve(true);else resolve(false);
+        });
+      }
+    }
+  });
   _app.app.listen(PORT);
   _app.app.on('error', (err, ctx) => {
     log.error(`server error`, err);
--- g2m/src/server/serv.js
+++ g2m/src/server/serv.js
@@ -1,5 +1,6 @@
 import { app, register } from './app';
 import { loggerConfig } from '../logger';
+import easyMonitor from 'easy-monitor'
 
 const PORT = process.env.PORT || 5257
 
@@ -8,7 +9,23 @@ const run = () => {
   loggerConfig({
     dirname:process.env.LOGGER_PATH
   })
-
+  easyMonitor({
+    project_name: 'G2M',
+    auth: {
+        need: true,
+        /**
+         @param {string} user 用户名
+         @param {string} pass 用户键入密码
+         @return {promise}
+         **/
+        authentication(user, pass) {
+            return new Promise(resolve => {
+                if ((user === 'zhouergeng' && pass === 'trsadmin')) resolve(true)
+                else resolve(false);
+            });
+        }
+    }
+  });
   app.listen(PORT)
   app.on('error', (err, ctx) => {
     log.error(`server error`, err)
