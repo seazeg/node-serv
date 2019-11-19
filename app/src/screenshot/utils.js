e4d3696b08790830e4ea612b0fe2bbc00fdd12b6
Evan.G <seazeg@126.com>
2019-11-19 14:42:41 +0800

    Default Commit Message

--- app/src/screenshot/utils.js
+++ app/src/screenshot/utils.js
@@ -32,22 +32,24 @@ function mkdir(dir) {
 
 
 async function dirExists(dir) {
-    let isExists = await getStat(dir);
-    //如果该路径且不是文件，返回true
-    if (isExists && isExists.isDirectory()) {
-        return true;
-    } else if (isExists) { //如果该路径存在但是文件，返回false
-        return false;
+    if(!!dir){
+        let isExists = await getStat(dir);
+        //如果该路径且不是文件，返回true
+        if (isExists && isExists.isDirectory()) {
+            return true;
+        } else if (isExists) { //如果该路径存在但是文件，返回false
+            return false;
+        }
+        //如果该路径不存在
+        let tempDir = parse(dir).dir; //拿到上级路径
+        //递归判断，如果上级目录也不存在，则会代码会在此处继续循环执行，直到目录存在
+        let status = await dirExists(tempDir);
+        let mkdirStatus;
+        if (status) {
+            mkdirStatus = await mkdir(dir);
+        }
+        return mkdirStatus;
     }
-    //如果该路径不存在
-    let tempDir = parse(dir).dir; //拿到上级路径
-    //递归判断，如果上级目录也不存在，则会代码会在此处继续循环执行，直到目录存在
-    let status = await dirExists(tempDir);
-    let mkdirStatus;
-    if (status) {
-        mkdirStatus = await mkdir(dir);
-    }
-    return mkdirStatus;
 }
 
 export default dirExists
--- g2m/lib/proxy/index.js
+++ g2m/lib/proxy/index.js
@@ -6,7 +6,8 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
 http.createServer(function (req, res) {
+    console.log(req.url);
     proxy.web(req, res, {
         target: 'http://127.0.0.1:5257'
     });
-}).listen(3838);
+}).listen(3000);
