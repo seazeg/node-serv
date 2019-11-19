faf1c0a653706a6542c7a9ca4136a5da7a42a5a8
Evan.G <seazeg@126.com>
2019-11-19 17:10:26 +0800

    Default Commit Message

--- g2m/src/proxy/index.js
+++ g2m/src/proxy/index.js
@@ -4,11 +4,33 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
 http.createServer(function (req, res) {
-    console.log(req.url);
-    proxy.web(req, res, {
-        target: 'http://127.0.0.1:5257'
+
+    if (req.url.includes('/api')) {
+        proxy.web(req, res, {
+            target: 'http://127.0.0.1:5257'
+        });
+    }
+
+    proxy.on('error', function (err, req, res) {
+        res.writeHead(500, {
+            'Content-Type': 'text/plain'
+        });
+
+        res.end('Something went wrong. And we are reporting a custom error message.');
     });
+
+    proxy.on('proxyRes', function (proxyRes, req, res) {
+        console.log(proxyRes.statusCode);
+    });
+
+    proxy.on('open', function (proxySocket) {
+        proxySocket.on('data', hybiParseAndLogMessage);
+    });
+
+    proxy.on('close', function (res, socket, head) {
+        console.log('Client disconnected');
+    });
+  
 }).listen(3000);
 
-
-
+console.log('proxy server was opened!');
